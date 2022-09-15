981895f1a677650eaf147478862afcddb8e1c912
Amr <akkk33@protonmail.com>
2022-09-15 08:28:25 +0200

    Up

--- src/store/theme.js
+++ src/store/theme.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
-import { browser } from '$app/env';
+import { browser } from '$app/environment';
 
 const ls = browser && localStorage.getItem('sveltegram-theme');
 
