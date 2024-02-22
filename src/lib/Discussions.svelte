0de9676099aa3eb83d9199846b7cd424f2a4cb80
Amr <akkk33@protonmail.com>
2024-02-22 04:54:11 +0200

    Migrated Discussions to svelte 5

--- src/lib/Discussions.svelte
+++ src/lib/Discussions.svelte
@@ -1,65 +1,66 @@
 <script>
 	import { onMount } from 'svelte';
 
-	/** @type {string}*/
-	export let link;
+	/**
+	 * @type {{
+	 * link: string;
+	 * pageURL: string;
+	 * color: string;
+	 * colorDark: string;
+	 * colorfulNames: boolean;
+	 * darkMode: boolean;
+	 * commentsLimit: number;
+	 * height: number | undefined;
+	 * }}
+	 */
+	let {
+		link,
+		pageURL,
+		color = '#2f81f6',
+		colorDark = '#89baff',
+		colorfulNames = false,
+		darkMode = false,
+		commentsLimit = 5,
+		height = undefined
+	} = $props();
+
+	/** @type {HTMLScriptElement | undefined}*/
+	let script = $state();
+	let telegramPost = $derived(link.replace('https://t.me/', ''));
 
 	/** @type {string}*/
-	export let pageURL = '';
-
-	/** @type {string}*/
-	export let color = '#2f81f6';
-
-	/** @type {string}*/
-	export let colorDark = '#89baff';
-
-	/** @type {boolean}*/
-	export let colorfulNames = false;
-
-	/** @type {boolean}*/
-	export let darkMode = false;
-
-	/** @type {number}*/
-	export let commentsLimit = 5;
-
-	/** @type {number|undefined}*/
-	export let height = undefined;
-
-	/** @type {HTMLScriptElement}*/
-	let script;
-	$: telegramPost = link.replace('https://t.me/', '');
-
-	/** @type {string}*/
-	let iFrameSource;
-	$: {
+	let iFrameSource = $state('');
+	$effect(() => {
 		iFrameSource = `${link}?embed=1&discussion=1&page_url=${pageURL}&comments_limit=${commentsLimit}${
 			darkMode ? '&dark=1' : ''
 		}&color=${color.replace('#', '')}&dark_color=${colorDark.replace('#', '')}${
 			colorfulNames ? '&colorful=1' : ''
 		}${height ? `&height=${height}` : ''}`;
 		iFrameStuff();
-	}
+	});
 
-	/** @type {HTMLDivElement}*/
-	let div;
+	/** @type {HTMLDivElement | undefined}*/
+	let div = $state();
 
 	function cleanStart() {
 		try {
-			div.innerHTML = '';
-			script = document.createElement('script');
-			script.src = 'https://telegram.org/js/telegram-widget.js?19';
-			script.setAttribute('async', 'true');
-			script.setAttribute('data-telegram-discussion', telegramPost);
-			script.setAttribute('data-color', color.replace('#', ''));
-			script.setAttribute('data-dark-color', colorDark.replace('#', ''));
-			script.setAttribute('data-colorful', `${colorfulNames ? '1' : '0'}`);
-			script.setAttribute('data-dark', `${darkMode}`);
-			script.setAttribute('data-comments-limit', `${commentsLimit}`);
-			if (height !== undefined) {
-				// The default height is set to "auto";
-				script.setAttribute('data-height', `${height}`);
+			if (div) {
+				div.innerHTML = '';
+				script = document.createElement('script');
+				script.src = 'https://telegram.org/js/telegram-widget.js?19';
+				script.setAttribute('async', 'true');
+				script.setAttribute('data-telegram-discussion', telegramPost);
+				script.setAttribute('data-color', color.replace('#', ''));
+				script.setAttribute('data-dark-color', colorDark.replace('#', ''));
+				script.setAttribute('data-colorful', `${colorfulNames ? '1' : '0'}`);
+				script.setAttribute('data-dark', `${darkMode}`);
+				script.setAttribute('data-comments-limit', `${commentsLimit}`);
+				if (height !== undefined) {
+					// The default height is set to "auto";
+					script.setAttribute('data-height', `${height}`);
+				}
+				div.appendChild(script);
 			}
-			div.appendChild(script);
 		} catch (e) {
 			console.error(e);
 		}
@@ -67,9 +68,11 @@
 
 	function iFrameStuff() {
 		try {
-			const iframe = div.getElementsByTagName('iframe')[0];
-			if (iframe) {
-				iframe.src = iFrameSource;
+			if (div) {
+				const iframe = div.getElementsByTagName('iframe')[0];
+				if (iframe) {
+					iframe.src = iFrameSource;
+				}
 			}
 		} catch (e) {
 			// iFrame is not loaded yet
