7647c0b9d46f5e3e72aa9dec84bd6436d8924a8e
Amr <akkk33@protonmail.com>
2025-01-23 21:41:50 +0200

    css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,7 +34,5 @@
 		background-color: var(--bg-clr);
 		color: var(--primary-clr);
 		box-shadow: 2px 5px 20px hsl(0 0% 20% / 0.3);
-		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',
-			Geneva, Verdana, sans-serif;
 	}
 </style>
